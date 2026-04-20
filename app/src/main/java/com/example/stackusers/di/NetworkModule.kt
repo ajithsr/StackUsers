fbc42f1e31431a0219d89f09d5a360d9b5d18d3a
Ajith Rajan <ajithsrajan@gmail.com>
2026-04-20 14:04:18 +0100

    feat: add Retrofit, OkHttpClient builder and di network module

---
+++ app/src/main/java/com/example/stackusers/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.example.stackusers.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://api.stackexchange.com/2.3/"
+
+    private val cacheSize = 10L * 1024 * 1024 // 10 MB
+
+    private val onlineInterceptor = Interceptor { chain ->
+        val response = chain.proceed(chain.request())
+        val maxAge = 60 * 5 // 5 minutes
+        response.newBuilder()
+            .header("Cache-Control", "public, max-age=$maxAge")
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
+        val cache = Cache(context.cacheDir, cacheSize)
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .addInterceptor(onlineInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+}
