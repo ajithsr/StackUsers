5cb9b22286b00ac0be14c94aef7021097b8aca5c
Ajith Rajan <ajithsrajan@gmail.com>
2026-04-22 00:38:39 +0100

    feat: add viewmodel method to toggle user follow status

--- app/src/main/java/com/example/stackusers/presentation/feature/userslist/UsersListViewModel.kt
+++ app/src/main/java/com/example/stackusers/presentation/feature/userslist/UsersListViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.stackusers.presentation.feature.userslist
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.stackusers.domain.model.Result
+import com.example.stackusers.domain.usecase.FollowUserUseCase
 import com.example.stackusers.domain.usecase.GetTopUsersUseCase
+import com.example.stackusers.domain.usecase.UnfollowUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,13 +14,19 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UsersListViewModel @Inject constructor(
-    private val getTopUsersUseCase: GetTopUsersUseCase
+    private val getTopUsersUseCase: GetTopUsersUseCase,
+    private val followUserUseCase: FollowUserUseCase,
+    private val unfollowUserUseCase: UnfollowUserUseCase
 ) : ViewModel() {
 
+    companion object {
+        private const val TAG = "UsersListViewModel"
+    }
     private val _uiState = MutableStateFlow<UiState<Any>>(UiState.Loading)
     val uiState: StateFlow<UiState<Any>> = _uiState.asStateFlow()
 
@@ -45,4 +54,14 @@ class UsersListViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
+    fun toggleFollow(userId: Long, isFollowing: Boolean) {
+        Log.d(TAG, "toggleFollow -> userId $userId   isFollowing: $isFollowing")
+        viewModelScope.launch {
+            if (isFollowing) {
+                unfollowUserUseCase(userId)
+            } else {
+                followUserUseCase(userId)
+            }
+        }
+    }
 }
