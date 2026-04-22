5a4007ed3159df097f097bf0cf3c4fb7ce28b116
Ajith Rajan <ajithsrajan@gmail.com>
2026-04-22 01:03:34 +0100

    refactor: clean up unused viewmodel and imports

--- app/src/main/java/com/example/stackusers/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/stackusers/presentation/ui/MainActivity.kt
@@ -4,50 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.example.stackusers.R
-import com.example.stackusers.domain.model.User
-import com.example.stackusers.presentation.feature.userslist.UiState
 import com.example.stackusers.presentation.feature.userslist.UsersListScreen
-import com.example.stackusers.presentation.feature.userslist.UsersListViewModel
 import com.example.stackusers.presentation.theme.StackUsersTheme
 import dagger.hilt.android.AndroidEntryPoint
-import java.text.NumberFormat
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val usersListViewModel: UsersListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
